f8b7d5742ce6743585a453d97b12808b32f57c6a
tedbeast <theodore.balashov@revature.com>
2024-04-01 02:40:36 -0400

    api change...

--- src/service/WorkspaceAPIService.tsx
+++ src/service/WorkspaceAPIService.tsx
@@ -1,14 +1,16 @@
 import { Workspace } from "../models/Workspace";
 
-const apiBase = "http://172.191.162.100:9002/"
+const apiBase = "http://172.191.162.100:9002"
 
 export function getWorkspace(workspace:Workspace){
     return fetch(apiBase+"/workspace",{
         method:"POST",
         mode:"cors",
         headers: {
-            "content-type": "application/json",
-            "access-control-allow-origin": "*"
+            "Content-Type":"application/json",
+            "Access-Control-Allow-Origin":"*",
+            "Access-Control-Allow-Header":"*",
+            "Access-Control-Allow-Method":"GET, POST, PUT, OPTIONS, DELETE"
           },
         body:JSON.stringify(workspace)
     })
