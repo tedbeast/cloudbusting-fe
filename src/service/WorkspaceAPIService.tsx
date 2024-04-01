1ac81176b47457431ac349079fd4adb5cd734279
tedbeast <theodore.balashov@revature.com>
2024-04-01 00:55:24 -0400

    api

--- src/service/WorkspaceAPIService.tsx
+++ src/service/WorkspaceAPIService.tsx
@@ -1,6 +1,6 @@
 import { Workspace } from "../models/Workspace";
 
-const apiBase = "http://172.191.162.100:3000/"
+const apiBase = "http://172.191.162.100:9002/"
 
 export function getWorkspace(workspace:Workspace){
     return fetch(apiBase+"/workspace",{
